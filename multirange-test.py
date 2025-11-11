9369ca301b81c539daf7f5cbd6943ce7a8df7af2
Gabriel Clima <gabriel.clima@gcore.com>
2025-11-11 10:33:33 +0000

    multirange test

---
+++ multirange-test.py
@@ -0,0 +1,48 @@
+import requests
+import re
+import random
+
+
+
+comp = True
+#uri = 'f2p24b'
+uri = 'f100m'
+boundary_pattern = rb'--[0-9]{20}'
+range_start = 'bytes='
+#max_range = 16777216
+max_range = 100000000//10
+
+ORIGIN='http://pwnrzclb.net/'
+CACHE='http://pwnrz-fe-preprod.gcdn.co/'
+
+while (comp):
+
+    a , b = sorted(random.sample(range(max_range), 2))
+    c , d = sorted(random.sample(range(max_range-(b-a)), 2))
+
+    range_h = f"{range_start}{a}-{b},{c}-{d}"
+    rheader = { 'Range' : range_h }
+
+    print(range_h)
+
+    r = requests.get(f"{ORIGIN}{uri}", headers=rheader)
+    rh = r.headers
+    print(r.status_code, r.elapsed.total_seconds(), len(r.content), rh['Content-length'])
+
+    mrheader = { 'Range' : range_h, 'Host': 'sliced'}
+    mr = requests.get(f"{CACHE}{uri}", headers=rheader)
+    mrh = mr.headers
+    print(mr.status_code, mr.elapsed.total_seconds(), len(mr.content), mrh['Cache'], mrh['traceparent'])
+
+    modified_r = re.sub(boundary_pattern, b'--BOUNDARY', r.content)
+    modified_mr = re.sub(boundary_pattern, b'--BOUNDARY', mr.content)
+    comp =  modified_r == modified_mr
+    print(comp)
+
+    if not (comp):
+        with open('/tmp/origin.txt', 'wb') as originc:
+            originc.write(r.content)
+        with open('/tmp/cache.txt', 'wb') as cachec:
+            cachec.write(mr.content)
+
+    print()
