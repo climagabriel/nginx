162fdee33da76c464f0dd227afc215d28f12d50e
Gabriel Clima <gabriel.clima@gcore.com>
2025-11-13 15:39:21 +0000

    more test improvements

--- multirange-test.py
+++ multirange-test.py
@@ -1,14 +1,28 @@
 import requests
 import re
 import random
+import signal
 import subprocess
+from pathlib import Path
 
+quit_received = False
+two_o_sixes = 0
+two_o_os = 0
+
+def signal_handler(sig, frame):
+    global quit_received
+    print("206/200: ", two_o_sixes, two_o_os)
+    quit_received = True
+
+signal.signal(signal.SIGINT, signal_handler)
+
+cache_dir = Path('/mnt/disk1/cache/')
 
 ORIGIN='http://127.0.0.1:8080/'
 CACHE='http://sliced/'
 uri = 'f2p24b'
 origin_file_size = 16777216
-max_distance = origin_file_size//10
+max_distance = origin_file_size//25
 
 boundary_pattern = rb'--[0-9]{20}'
 range_start = 'bytes='
@@ -16,7 +30,8 @@ range_start = 'bytes='
 
 c = 0
 comp = True
-while (comp):
+
+while (comp and not quit_received):
     range_header = range_start
     rangecount = random.randint(2,6)
 
@@ -50,6 +65,13 @@ while (comp):
     r = requests.get(f"{ORIGIN}{uri}", headers=rheader)
     rh = r.headers
     print(r.status_code, r.elapsed.total_seconds(), len(r.content), rh['Content-length'])
+    if(r.status_code == 206):
+        two_o_sixes += 1
+    elif (r.status_code == 200):
+        two_o_os += 1
+    else:
+        print(r.status_code, range_header)
+        quit()
 
     mr = requests.get(f"{CACHE}{uri}", headers=rheader)
     mrh = mr.headers
@@ -59,7 +81,7 @@ while (comp):
     modified_r = re.sub(boundary_pattern, b'--BOUNDARY', r.content)
     modified_mr = re.sub(boundary_pattern, b'--BOUNDARY', mr.content)
     comp =  modified_r == modified_mr
-    print(comp)
+    print(comp, '\n')
 
     if not (comp):
         with open('/tmp/origin.txt', 'wb') as originc:
@@ -68,7 +90,9 @@ while (comp):
             cachec.write(mr.content)
         quit()
 
-    print()
 
     if (random.choice([True, False])):
-        subprocess.run('find /mnt/disk1/cache/ -type f -delete', shell=True, check=True)
+        subprocess.run('find /mnt/disk1/cache/ -type f -delete', shell=True, check=True) #faster
+       # for file_path in cache_dir.rglob('*'):
+       #     if file_path.is_file():
+       #         file_path.unlink()
